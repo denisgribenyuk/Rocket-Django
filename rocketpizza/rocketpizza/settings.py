fc595b77b9d573a7e1cf613363508f3b6f912251
denisgribenuk <usere23caA>
2022-02-26 19:37:08 +0300

    Done v.1

--- rocketpizza/rocketpizza/settings.py
+++ rocketpizza/rocketpizza/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8vctx1i$smdkf_jg5vhbfx4@zo4jnwh1k=-shl1r9_33sb_$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
